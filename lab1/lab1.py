415942bf07ed48d93a804996d1eae5842acc8c89
Georgy <georgiisafonk@gmail.com>
2025-03-10 17:36:11 +0700

    lab1 update

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,62 +1,61 @@
-import numpy as np
 import math
-import scipy
+
+import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats
-
 
 A = scipy.stats.uniform.rvs(loc=-3, scale=6)
 B = scipy.stats.uniform.rvs(loc=-3, scale=6)
 C = scipy.stats.uniform.rvs(loc=-3, scale=6)
 D = scipy.stats.uniform.rvs(loc=-3, scale=6)
 EPSILON_0 = 1
-POINTS_NUMBER = 25
-POINTS_NUMBER_FOR_FULL = 100
+POINTS_NUMBER = 64
+POINTS_NUMBER_FOR_FULL = 128
+POLY_DEGREE = 5
+F1_STR = "A*x^3 + B*x^2 + C*x + D"
+F2_STR = "x*sin(2*pi*x)"
 
-
-def F1(x : float) -> float:
+def f1(x : float) -> float:
     return A*np.pow(x, 3) + B*np.pow(x, 2) + C*x + D
 
-
-def F2(x : float) -> float:
-    return x * math.sin(math.pi * x)
-
+def f2(x : float) -> float:
+    return x * math.sin(2 * math.pi * x)
 
 uniform_mistakes = np.random.uniform(-EPSILON_0, EPSILON_0, POINTS_NUMBER)
 normal_mistakes = np.random.normal(0, EPSILON_0, POINTS_NUMBER)
 
 # Сетка для формирования выборки
-grid = np.linspace(-1, 1, POINTS_NUMBER)
+grid = np.random.uniform(-1, 1, POINTS_NUMBER)
 
 # Сетка для построения графиков функций
 full_points = np.linspace(-1, 1, POINTS_NUMBER_FOR_FULL)
 
-# Формирование данных 
-y = [F1(full_points[i] )for i in range(POINTS_NUMBER_FOR_FULL)]
+# Получение точек исходной функции
+y = [f2(full_points[i]) for i in range(POINTS_NUMBER_FOR_FULL)]
 
-n_data = list()
-u_data = list()
+n_data = []
+u_data = []
 
-
-for i in range(len(grid)):
-    n_data.append(F1(grid[i]) + normal_mistakes[i])
-    u_data.append(F1(grid[i]) + uniform_mistakes[i])
+# Получение точек выборки для двух распределений ошибки
+for i, point in enumerate(grid):
+    n_data.append(f2(point) + normal_mistakes[i])
+    u_data.append(f2(point) + uniform_mistakes[i])
 
 normal_sample = np.array(n_data)
 uniform_sample = np.array(u_data)
 
-
 # Полиномиальная регрессия (метод наименьших квадратов)
-coefficients = np.polyfit(grid, n_data, 3)
+coefficients = np.polyfit(grid, n_data, POLY_DEGREE)
 poly = np.poly1d(coefficients)
 
-
-plt.plot(full_points, y, linestyle='-', color='b', label=f"Исходная функция")  # Синие точки + линия
-plt.scatter(grid, uniform_sample, marker='o', linestyle='-', color='r', label=f"Точки выборки с ошибкой")  # Синие точки + линия
-plt.plot(full_points, poly(full_points), color='g', label=f'Восстановленная функциональная зависимость')
+# Отрисовка
+plt.plot(full_points, y, linestyle='-', color='b', label=f"Исходная функция {F2_STR}")
+plt.scatter(grid, normal_sample, marker='o', linestyle='-', color='r', label=f"Точки выборки")
+plt.plot(full_points, poly(full_points), color='g', label=f"Восстановленный полином {POLY_DEGREE} степени")
 plt.xlabel("x")
 plt.ylabel("y")
-plt.title("Пример работы полиномиальной регрессии")
+plt.title("Полиномиальная регрессия. Хорошее предсказание.")
 plt.legend()
 plt.grid()
-plt.savefig("plot.png")
+plt.ylim(-6, 6)
+plt.savefig(f"polyregr_xsin2pix_optimal.png")
